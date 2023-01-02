38e9083885a9674186f31cb7485ddc0da06aba91
ZannBuce <zannbuce@gmail.com>
2023-01-02 21:48:50 +0800

    Settings update

--- src/views/dashboard/Settings.jsx
+++ src/views/dashboard/Settings.jsx
@@ -1,14 +1,95 @@
 import { Typography,useTheme } from "@mui/material";
 import { tokens } from "../../theme";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+const studentInfo = [  {    firstname: "John",    lastname: "Doe", birthday: "April 19, 2002", mobilenumber: "09123456789", email: "zannbuce@gmail.com", password: "a********p" }];
+
+const studentName = studentInfo.map((student) => {
+  return student.firstname, student.lastname, student.birthday, student.mobilenumber, student.email, student.password
+})
+
 
 const Settings = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-return (
- //Your code here
- <Typography>Users Settings</Typography>
-)
-}
+ 
+
+  return (
+    //Your code here
+<div>
+
+    <Box>
+      
+
+    <Typography>{studentInfo.map(student => {
+      
+        return (
+            <div>
+              <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          label="First Name"
+          id="outlined-size-normal"
+          defaultValue={student.firstname}
+          
+        />
+        <TextField 
+        label="Last Name"
+        id="outlined-size-normal" 
+        defaultValue={student.lastname} 
+        />
+        </div>
+        <div>
+        <TextField 
+        label="Birthday"
+        id="outlined-size-normal" 
+        defaultValue={student.birthday} 
+        />
+        <TextField 
+        label="Mobile Number"
+        id="outlined-size-normal" 
+        defaultValue={student.mobilenumber}
+        />
+        </div>
+        <div>
+        <TextField 
+        label="Email"
+        id="outlined-size-normal" 
+        defaultValue={student.email}
+        />
+        <TextField 
+        label="Password"
+        id="outlined-size-normal" 
+        defaultValue={student.password}
+        />
+        </div>
+
+        </Box>
+        </div>
+
+
+        )
+
+    })}
+    </Typography>
+    </Box>
+    </div>
+
+  );
+};
+
+
+
 
 export default Settings;
