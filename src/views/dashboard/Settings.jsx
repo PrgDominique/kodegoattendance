1cf8505aa4b117a28be26d2db06ef5c057db2745
ZannBuce <zannbuce@gmail.com>
2023-01-04 20:05:20 +0800

    Update settings

--- src/views/dashboard/Settings.jsx
+++ src/views/dashboard/Settings.jsx
@@ -1,52 +1,123 @@
-import { Typography, useTheme } from "@mui/material";
+import { Typography,useTheme } from "@mui/material";
 import { tokens } from "../../theme";
-import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
-  
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
-  
-   function Settings() {
-    return (
-      <div style={{ height: 400, width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-          checkboxSelection
+
+
+const studentInfo = [  {    firstname: "John",    lastname: "Doe", birthday: "December 19, 1900", mobilenumber: "09123456789", email: "johndoe@gmail.com", batch: "WD29" }];
+
+const studentName = studentInfo.map((student) => {
+  return student.firstname, student.lastname, student.birthday, student.mobilenumber, student.email, student.batch
+})
+
+
+const Settings = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+ 
+
+  return (
+    //Your code here
+
+
+    <Container Fluid>
+<div>
+<Box>
+
+    <Typography>{studentInfo.map(student => {
+      
+        return (
+            <div>
+              <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          label="First Name"
+          id="outlined-size-normal"
+          defaultValue={student.firstname}
+          
         />
+        <TextField 
+        label="Last Name"
+        id="outlined-size-normal" 
+        defaultValue={student.lastname} 
+        />
+        </div>
+        <div>
+        <TextField 
+        label="Birthday"
+        id="outlined-size-normal" 
+        defaultValue={student.birthday} 
+        />
+        <TextField 
+        label="Mobile Number"
+        id="outlined-size-normal" 
+        defaultValue={student.mobilenumber}
+        />
+        </div>
+        <div>
+        <TextField 
+        label="Email"
+        id="outlined-size-normal" 
+        defaultValue={student.email}
+        />
+        <TextField 
+        label="Batch"
+        id="outlined-size-normal" 
+        defaultValue={student.batch}
+        />
+        </div>
+
+
+    <Box>
+      <div>
+      <TextField 
+      fullWidth label="Password" 
+      id="fullWidth" 
+      placeholder="sx"
+      sx={{
+        width: { sm: 200, ms: 300 },
+        "& .MuiInputbase-root": {
+          height: 80
+        },
+        mb: 2
+      }}
+      />
       </div>
-    );
-  }
+      <div>
+      <TextField fullWidth label="Confirm Password" id="fullWidth" />
+      </div>
+      
+    </Box>
+  
+
+
+        </Box>
+        </div>
+
+
+        )
+
+    })}
+    </Typography>
+    </Box>
+    </div>
+</Container>
+
+
+  );
+};
+
+
+
 
 export default Settings;
