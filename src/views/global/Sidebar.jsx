ce231e997718c3c6c09dda5492e86d2039895d20
maturaofficial <maturaofficial@gmail.com>
2023-01-17 00:08:30 +0800

    Change console text

--- src/views/global/Sidebar.jsx
+++ src/views/global/Sidebar.jsx
@@ -58,7 +58,7 @@ const Sidebar = () => {
         setBatchID(snapshot.val().batchID);
       });
     } else {
-      console.log("No user is signed in.")
+      console.log("WAIT.")
     }
 
   }, [auth.currentUser]);
