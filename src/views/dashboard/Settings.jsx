474880e62cb8ac45d6ea7972c2a0fd161f321e65
PrgDominique <dominiquesimbillo10@gmail.com>
2023-01-17 09:11:57 +0800

    q

--- src/views/dashboard/Settings.jsx
+++ src/views/dashboard/Settings.jsx
@@ -6,57 +6,58 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import { useEffect, useState } from "react";
-import { getDatabase, ref, set, equalTo, child,get } from "firebase/database";
-
+import { getDatabase, ref, child, get, update } from "firebase/database";
 
 import { auth } from "../../firebase";
 import { UserAuth } from "../../context/AuthContext";
 
 const Settings = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [mobile, setMobileNumber] = useState("");
-    const [email, setEmail] = useState("");
-    const [birthDate, setBirthDate] = useState("");
-    const [batchID, setBatchID] = useState("");
-    const [edit, setEdit] = useState(true);
-    const db = getDatabase();
-    const {  } = UserAuth();
-    
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [mobile, setMobileNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [batchID, setBatchID] = useState("");
+  const [edit, setEdit] = useState(true);
+  const db = getDatabase();
+  const {} = UserAuth();
 
-    const handleUpdate = () => {
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const userId = auth.currentUser.uid;
+    const userRef = ref(db, "users");
+    update(child(userRef, `${userId}`), {
+      firstName: firstName,
+      lastName: lastName,
+      mobile: mobile,
+      birthDate: birthDate,
+    });
+  };
 
-       
+  useEffect(() => {
+    if (auth.currentUser) {
+      const userId = auth.currentUser.uid;
+      const userRef = ref(db, "users");
+      get(child(userRef, `${userId}`)).then((snapshot) => {
+        setFirstName(snapshot.val().firstName);
+        setLastName(snapshot.val().lastName);
+        setMobileNumber(snapshot.val().mobile);
+        setEmail(snapshot.val().email);
+        setBirthDate(snapshot.val().birthDate);
+        setBatchID(snapshot.val().batchID);
+      });
+    } else {
+      console.log("No user is signed in.");
     }
+  }, [auth.currentUser]);
 
-
-    useEffect(() => {
-        if (auth.currentUser) {
-    
-          const userId = auth.currentUser.uid;
-          const userRef = ref(db, 'users');
-          get(child(userRef, `${userId}`)).then((snapshot) => {
-            setFirstName(snapshot.val().firstName);
-            setLastName(snapshot.val().lastName);
-            setMobileNumber(snapshot.val().mobile);
-            setEmail(snapshot.val().email);
-            setBirthDate(snapshot.val().birthDate);
-            setBatchID(snapshot.val().batchID);
-          });
-        } else {
-          console.log("No user is signed in.")
-        }
-    
-      }, [auth.currentUser]);
-
-
-return (
- //Your code here
-<>
-<Typography variant="h4">
+  return (
+    //Your code here
+    <>
+      <Typography variant="h4">
         <Box
           sx={{
             width: 1200,
@@ -145,114 +146,147 @@ return (
                 >
                   Edit Account
                 </Button>
+
+                <Button
+                  variant="outlined"
+                  color="success"
+                  sx={{
+                    marginTop: 10,
+                    marginLeft: 10,
+                    height: 60,
+                    width: 180,
+                    fontSize: 16,
+                  }}
+                >
+                  Upload Profile
+                </Button>
+
+                <Button
+                  variant="outlined"
+                  color="success"
+                  sx={{
+                    marginTop: 10,
+                    marginLeft: 10,
+                    height: 60,
+                    width: 180,
+                    fontSize: 16,
+                  }}
+                >
+                  Change Password
+                </Button>
               </Container>
             </Container>
           ) : (
             <Container maxWidth="lg" sx={{}}>
               <form onSubmit={handleUpdate}>
-              <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                  <h4>First Name</h4>
-                  <TextField
-                    label="First Name"
-                    id="outlined-size-normal"
-                    onChange={(e) => setFirstName(e.target.value)}
-                    value={firstName}
-                    sx={{ width: 250 }}
-                  />
-                </Grid>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} md={6}>
+                    <h4>First Name</h4>
+                    <TextField
+                      label="First Name"
+                      id="outlined-size-normal"
+                      onChange={(e) => setFirstName(e.target.value)}
+                      value={firstName}
+                      sx={{ width: 250 }}
+                    />
+                  </Grid>
 
-                <Grid item xs={12} md={6}>
-                  <h4>Last Name</h4>
-                  <TextField
-                    label="Last Name"
-                    id="outlined-size-normal"
-                    onChange={(e) => setLastName(e.target.value)}
-                    value={lastName}
-                    sx={{ width: 250 }}
-                  />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <h4>Mobile Number</h4>
-                  <TextField
-                    label="Mobile Number"
-                    id="outlined-size-normal"
-                    onChange={(e) => setMobileNumber(e.target.value)}
-                    value={mobile}
-                    sx={{ width: 250 }}
-                  />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <h4>Email</h4>
-                  <TextField
-                    label="Email"
-                    id="outlined-size-normal"
-                    value={email}
-                    disabled
-                    sx={{ width: 250 }}
-                  />
-                </Grid>
+                  <Grid item xs={12} md={6}>
+                    <h4>Last Name</h4>
+                    <TextField
+                      label="Last Name"
+                      id="outlined-size-normal"
+                      onChange={(e) => setLastName(e.target.value)}
+                      value={lastName}
+                      sx={{ width: 250 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <h4>Mobile Number</h4>
+                    <TextField
+                      label="Mobile Number"
+                      id="outlined-size-normal"
+                      onChange={(e) => setMobileNumber(e.target.value)}
+                      value={mobile}
+                      sx={{ width: 250 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <h4>Email</h4>
+                    <TextField
+                      label="Email"
+                      id="outlined-size-normal"
+                      value={email}
+                      disabled
+                      sx={{ width: 250 }}
+                    />
+                  </Grid>
 
-                <Grid item xs={12} md={6}>
-                  <h4>Birth Date</h4>
-                  <TextField
-                    label="Date of Birth"
-                    id="outlined-size-normal"
-                    onChange={(e) => setBirthday(e.target.value)}
-                    value={birthDate}
-                    sx={{ width: 250 }}
-                  />
+                  <Grid item xs={12} md={6}>
+                    <h4>Birth Date</h4>
+                    <TextField
+                      label="Date of Birth"
+                      id="outlined-size-normal"
+                      onChange={(e) => setBirthDate(e.target.value)}
+                      value={birthDate}
+                      sx={{ width: 250 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <h4>Batch</h4>
+                    <TextField
+                      label="Batch"
+                      id="outlined-size-normal"
+                      onChange={(e) => setBatchID(e.target.value)}
+                      value={batchID}
+                      sx={{ width: 250 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <Button variant="contained" component="label">
+                      Upload File
+                      <input type="file" hidden />
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid item xs={12} md={6}>
-                  <h4>Batch</h4>
-                  <TextField
-                    label="Batch"
-                    id="outlined-size-normal"
-                    onChange={(e) => setBatchID(e.target.value)}
-                    value={batchID}
-                    sx={{ width: 250 }}
-                  />
-                </Grid>
-              </Grid>
-              <Container>
-                <Button
-                  variant="outlined"
-                  type="submit"
-                  color="success"
-                  onClick={() => setEdit(!edit)}
-                  sx={{
-                    marginTop: 10,
-                    marginLeft: 5,
-                    height: 60,
-                    width: 180,
-                    fontSize: 16,
-                  }}
-                >
-                  Apply Now
-                </Button>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  onClick={() => setEdit(!edit)}
-                  sx={{
-                    marginTop: 10,
-                    marginLeft: 5,
-                    height: 60,
-                    width: 180,
-                    fontSize: 16,
-                  }}
-                >
-                  Cancel
-                </Button>
-              </Container>
-                </form>
+                <Container>
+                  <Button
+                    variant="outlined"
+                    type="submit"
+                    color="success"
+                    onClick={handleUpdate}
+                    sx={{
+                      marginTop: 10,
+                      marginLeft: -3,
+                      height: 60,
+                      width: 180,
+                      fontSize: 16,
+                    }}
+                  >
+                    Apply Now
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => setEdit(!edit)}
+                    sx={{
+                      marginTop: 10,
+                      marginLeft: 5,
+                      height: 60,
+                      width: 180,
+                      fontSize: 16,
+                    }}
+                  >
+                    Close
+                  </Button>
+                </Container>
+              </form>
             </Container>
           )}
         </Box>
       </Typography>
-
-</>
-)
-}
+    </>
+  );
+};
 
 export default Settings;
